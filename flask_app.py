a84a600241106db6e5470fa384af289b4a451c52
= <=>
2022-07-05 03:25:29 +0100

    update

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,50 @@
 
 
+import itertools
 import sys
 from flask import Flask, render_template, request
+
+
+
+
+
 
 app = Flask(__name__)
 
 @app.route('/fpvecalculator')
-def hello_world():
+def fpvecalculator():
     return render_template('index.html')
 
-@app.route('/')
-def example_route():
-    return 'Example route here!!!'
+
+@app.route('/uolfp')
+def uolfp():
+    return str(floyd(graph))
+
+NO_PATH = sys.maxsize
+
+graph = [[0, 7, NO_PATH, 8],
+[NO_PATH, 0, 5, NO_PATH],
+[NO_PATH, NO_PATH, 0, 2],
+[NO_PATH, NO_PATH, NO_PATH, 0]]
+MAX_LENGTH = len(graph[0])
+
+def floyd(distance):
+    """
+    A simple implementation of Floyd's algorithm
+    """
+    for intermediate, start_node,end_node\
+        in itertools.product\
+            (range(MAX_LENGTH),range(MAX_LENGTH), range(MAX_LENGTH)):
+        # Assume that if start_node and end_node are the same
+        # then the distance would be zero
+        if start_node == end_node:
+            distance[start_node][end_node] = 0
+            continue
+        #return all possible paths and find the minimum
+        distance[start_node][end_node] = min(distance[start_node][end_node],
+            distance[start_node][intermediate] + distance[intermediate][end_node] )
+        #Any value that have sys.maxsize has no path
+    return distance
 
 # Recursion of Graph
 def graph_recursion(rv,OV,graph,MAX_LENGTH,NO_PATH):
